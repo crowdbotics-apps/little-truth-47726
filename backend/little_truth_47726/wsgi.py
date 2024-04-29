d2cbb722bb7a5c888dc368b0fa611d9b418ca071
Crowdbotics <team@crowdbotics.com>
2024-04-29 00:36:35 +0000

    Initial commit

---
+++ backend/little_truth_47726/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for little_truth_47726 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "little_truth_47726.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-truth-47726",
+  url: "https://little-truth-47726.botics.co",
+  api: "https://little-truth-47726.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
